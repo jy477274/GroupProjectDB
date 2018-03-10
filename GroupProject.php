c686bf267d66bc3e1a1e7cda05256a78a0f66a92
jy477274 <32715627+jy477274@users.noreply.github.com>
2018-03-09 21:19:37 -0400

    Add files via upload

---
+++ GroupProject.php
@@ -0,0 +1,85 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <form>
+      <fieldset>
+        <!-- First Name -->
+       <label>User Information</label>
+        <div class="input-group">
+          <div class="input-group-prepend">
+            <span class="input-group-text" id="First Name">First Name</span>
+          </div>
+          <input type="text" class="form-control">
+        </div><br>
+
+      <!-- Last Name -->
+      <div class="input-group">
+        <div class="input-group-prepend">
+          <span class="input-group-text" id="Last Name">Last Name</span>
+        </div>
+        <input type="text" class="form-control">
+      </div><br>
+
+      <!-- Username -->
+      <div class="input-group">
+        <div class="input-group-prepend">
+          <span class="input-group-text" id="Username">Username</span>
+        </div>
+        <input type="text" class="form-control">
+      </div><br>
+
+      <!-- Email -->
+      <div class="input-group">
+        <div class="input-group-prepend">
+          <span class="input-group-text" id="Email">Email</span>
+        </div>
+        <input type="text" class="form-control">
+      </div><br>
+
+      <!-- Character Type -->
+      <div class="input-group mb-3">
+        <div class="input-group-prepend">
+          <label class="input-group-text" for="CharTypeSelect">Character Type</label>
+        </div>
+        <select class="custom-select" id="CharTypeSelect">
+          <option selected>Choose...</option>
+          <option value="Mage">Mage</option>
+          <option value="Fighter">Fighter</option>
+          <option value="Archer">Archer</option>
+          <option value="Rogue">Rogue</option>
+        </select><br>
+      </div>
+
+      <!-- Clan Input -->
+      <div class="input-group mb-3">
+        <div class="input-group-prepend">
+          <label class="input-group-text" for="ClanSelect">Clan</label>
+        </div>
+        <select class="custom-select" id="ClanSelect">
+          <option selected>Choose...</option>
+          <option value="Ziza">Ziza</option>
+          <option value="MLG">MLG</option>
+          <option value="42069360NS">42069360NS</option>
+          <option value="H@v0c">H@v0c</option>
+        </select>
+      </div><br>
+
+      <!-- Submit Button -->
+      <button type="submit" class="btn btn-primary">Submit</button>
+
+      </fieldset>
+    </form>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  </body>
+</html>
