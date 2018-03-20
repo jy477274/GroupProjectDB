8773c5bfadf7447d4301a9a378f3ec1a29950468
ryanwilbur <rwilbur633@gmail.com>
2018-03-20 15:57:35 -0300

    Revert "search user"
    This reverts commit bfb44a4b8a09d1ccd5ee1434b79adc90bd5afec7.

--- searchUser.php
+++ searchUser.php
@@ -1,15 +1,9 @@
-<?php
-include("backButton.php")
-
-?>
-
-
 
 <?php
 
   //create connection to MySQL database **CHANGE**
-  include 'testsql/pdo.php';
-  $pdo = new PDO($dsn, $user, $pass, $opt);
+  //include 'testsql/pdo.php';
+//  $pdo = new PDO($dsn, $user, $pass, $opt);
 
 
   //check connection
@@ -38,20 +32,82 @@ include("backButton.php")
 
 <!doctype html>
 <html lang="en">
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-  <head>
-    <!-- Required meta tags -->
-    <h1>Search User</h1>
-    <br>
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+  <head/>
+    <style>
+        header{
+            background:  #3b5998;
+            color: #ffffff;
+            padding-left: 0px;
+            padding-bottom: 30px;
+            min-height: 70px;
+            border-bottom: #8b9dc3 3px soli
+            width; 100%;
+            text-align: left;
+        }
+            body{
+                front 15px/1.5 Arial, Helvetica,sans-serif;
+                padding-top: 0px;
+                padding-left: 0px;
 
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+                background-color: #dfe3ee;
 
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+            }
+            footer{
+                position:absolute;
+                bottom: 0;
+                width: 100%;
+                hight:60px;
+                padding:20px;
+                margin-top: 20px;
+                color: #ffffff;
+                background-color:#3b5998;
+                border-top:  #8b9dc3 3px solid;
+                text-align: center;
+            }
+            .container{
+
+            }
+            h1{
+                margin: 0 auto;
+                text-align: left;
+                text-align: start;
+                padding-top: 20px;
+                padding-bottom: 20px;
+            }
+            h2{
+                padding-top: 70px;
+            }
+        }
+    </style>
     <body>
-    <form method="POST"  >
+        <header>
+            <div class="container">
+                    <div id="branding">
+                            <h1>DalCS Gamez</h1>
+                            <form method="POST" action="index.php">
+                                <fieldset>
+                                    <button type="submit" class="btn btn-primary">Back</button>
+                                </fieldset>
+                            </form>
+
+                    </div>
+            </div>
+        </header>
+        <section id="filename">
+            <div class="contatiner">
+                <h2>User Search</h2>
+            </div>
+        </section>
+    <form method="POST" action="UserEntryForm.php">
       <fieldset>
+
 
       <!-- Clan Input -->
       <div class="input-group mb-3">
@@ -59,28 +115,12 @@ include("backButton.php")
           <label class="input-group-text" for="SearchOption">Search By</label>
         </div>
 
-        <!-- search options -->
-        <select class="custom-select" name="SearchOption" id="SearchOption" onchange="funOption()" >
+        <select class="custom-select" name="SearchOption" id="SearchOption">
           <option selected value = null>Choose...</option>
-          <option id=FirstName value=FirstName>First Name</option>
-          <option id=LastName value=LastName>Last Name</option>
-          <option id=Username value=Username>Username</option>
-          <option id=Clan value=Clan>Clan</option>
-        </select>
-      </div><br>
-
-      <!--Clan Options -->
-      <div class="input-group mb-3">
-          <div class="input-group-prepend">
-            <label class="input-group-text" for="SearchOption">Clan Name</label>
-          </div>
-        <select class="custom-select" name="ClanSelect" id="ClanSelect">
-          <option selected value = null>Choose...</option>
-          <option value=Ziza>Ziza</option>
-          <option value=MLG>MLG</option>
-          <option value=42069360NS>42069360NS</option>
-          <option value=H@v0cc>H@v0cc</option>
-          <option value=Shakira>Shakira ( ͡° ͜ʖ ͡°)</option>
+          <option value=FirstName>First Name</option>
+          <option value=LastName>Last Name</option>
+          <option value=Username>Username</option>
+          <option value=Clan>Clan</option>
         </select>
       </div><br>
 
@@ -104,99 +144,28 @@ include("backButton.php")
     </form>
     <?php
     if (isset($_POST['Search'])){
-      $value = $_POST['SearchOption'];
+      $SearchBy = $_POST['SearchOption'];
       $Input =$_POST['Input'];
-      $Clanname =$_POST['ClanSelect'];
 
-      if ($value == "FirstName") {
-        $query = "call csgamez.searchUserByFirstName('$Input');";
-        $sth = $pdo->prepare($query);
-        $sth->execute();
-      }
-      elseif ($value == "LastName") {
-        $query = "call csgamez.searchUserByLastName('$Input');";
-        $sth = $pdo->prepare($query);
-        $sth->execute();
-      }
-      elseif ($value == "Username") {
-        $query = "call csgamez.searchUserByUsername('$Input');";
-        $sth = $pdo->prepare($query);
-        $sth->execute();
-      }
-      else{
-        $query = "call csgamez.searchUsersByClan('$Clanname');";
 
-        mysqli_query($pdo,$query);
 
-        echo "<table border='1'>
-        <tr>
-        <th>Firstname</th>
-        <th>Lastname</th>
-        </tr>";
 
-        while($row = mysqli_fetch_array($query))
-        {
-        echo "<tr>";
-        echo "<td>" . $row['FirstName'] . "</td>";
-        echo "<td>" . $row['LastName'] . "</td>";
-        echo "</tr>";
-        }
-        echo "</table>";
+      echo "Created : $FName,$LName,$Username,$Password,$Email,$ClanSelect";
 
-        mysqli_close($con);
+      $query = "call csgamez.createNewUser('$FName','$LName','$Username','$Password','$Email','$ClanSelect');";
+      $sth = $pdo->prepare($query);
 
-      }
-
+      $sth->execute();
     }
     ?>
-
-    <script>
-    function funOption() {
-      var value = document.getElementById("SearchOption").value;
-
-
-      if (value == "Clan") {
-        document.getElementById("Input").style.display = "none";
-        document.getElementById("ClanSelect").style.display = "block";
-      }
-      else{
-        document.getElementById("Input").style.display = "block";
-        document.getElementById("ClanSelect").style.display = "none";
-
-      }
-
-    }
-
-    </script>
-
-
 
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    <footer>
+        <p>Made with love by Jayden Macdonald, Ryan Wilbur, Dorukhan Calışkan, and Dylan Roberts</p>
+    </footer>
   </body>
 </html>
-
-
-<!--
-
-function funOption() {
-  var value = document.getElementById("SearchOption").value;
-  var current_value = dropdown.options[dropdown.selectedIndex].value;
-
-  document.getElementById("demo").innerHTML = 5+11;
-
-  if (value === "Clan") {
-      document.getElementById("Input").style.visibility="hidden";
-      document.getElementById("ClanSelect").style.visibility="visible";
-  }
-  else {
-      document.getElementById("Input").style.display = "none";
-      document.getElementById("ClanSelect").style.display = "block";
-
-  }
-}
-
--->
